c5abaa988a9ccbfda550ae61a0eefeadd0236d57
digitalligit@gmail.com <G17hub@)@!>
2023-10-31 21:49:11 -0300

    UPDATE: Presentaion improve

--- sender.py
+++ sender.py
@@ -6,7 +6,10 @@ from settings import AUTH_HOST
 def send_post_json(message: dict) -> dict:
     if AUTH_HOST:
         jsonfied = json.dumps(message)
-        response = requests.post(AUTH_HOST, data=jsonfied)
-        return json.loads(response.json())
+        response = requests.post(AUTH_HOST, headers={'Content-Type': 'application/json'}, data=jsonfied)
+        if response.status_code == 200:
+            content = "\n".join([f"    {k}: {v}" for k, v in json.loads(response.content).items()])
+            return f"STATUS {response.status_code}\n REPONSE:\n{content}\n\n"
+        return f"STATUS {response.status_code} REPONSE: {response.content}\n\n"
     else:
         return "ATENÇÃO: AUTH_HOST não configurado.\n\n"
